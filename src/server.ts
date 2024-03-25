614b805f52e20402c03ba492f87150a4c8457944
Madso Silva <madson@Madsos-iMac-Pro.local>
2024-03-25 12:19:54 -0300

    refactor: remove api service from docker-compose file

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,20 @@ import { setupSwagger } from './configs/swagger.config';
 const port = 8080;
 
 async function init(): Promise<void> {
-  
   const app = express();
 
-  app.use(express.json())
-  
-  app.use(cors({
-    origin: '*'
-  }))
+  app.use(express.json());
 
-  await setupRouter(app);
+  app.use(
+    cors({
+      origin: '*'
+    })
+  );
+
   await setupSwagger(app);
+  await setupRouter(app);
 
-  app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+  app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
 }
 
 init();
-
